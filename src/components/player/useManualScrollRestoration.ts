15eef40b10dce1d77e47e68abb3166c551588b9d
Chris Kahn <chris.a.kahn@gmail.com>
2026-04-26 22:38:12 -0700

    add comment

--- src/components/player/useManualScrollRestoration.ts
+++ src/components/player/useManualScrollRestoration.ts
@@ -2,6 +2,13 @@
 
 import { useEffect } from 'react';
 
+// We need this to prevent the browser from automatically scrolling to the top
+// of the page when the user scrolls up and then refreshes (e.g., mobile refresh
+// via drag-down gesture).
+//
+// The browser's default behavior is to scroll to the top of the page when the
+// user refreshes the page. But for episode details page, we want to keep the
+// last selected segment in view.
 export function useManualScrollRestoration(): void {
   useEffect(() => {
     if (typeof window === 'undefined' || !('scrollRestoration' in window.history)) {
